8b26957c54327e61dd648765674eac6cc90daaa0
Esteban Di Falco <estebandifalco@gmail.com>
2021-03-24 21:53:19 +0100

    Create Events widget that will be displayed in the main Neuronet webiste

---
+++ resources/views/events.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.embed')
+
+@section('content')
+    <div class="box box-purple">
+        <div class="box-header with-border">
+            <h3 class="box-title">Events</h3>
+
+            {{--    <div class="box-tools pull-right">--}}
+            {{--      <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>--}}
+            {{--      </button>--}}
+            {{--      <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>--}}
+            {{--    </div>--}}
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="table-responsive">
+                <table class="table no-margin">
+                    <thead>
+                    <tr>
+                        <th style="min-width: 85px;">Date</th>
+                        <th>Event</th>
+                        <th>Projects</th>
+                        <th>Location</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    @foreach($events as $event)
+                        <tr>
+                            <td>{{ $event->start_date }}</td>
+                            <td><a href="/admin/calendars/{{ $event->id }}">{{ $event->title }}</a></td>
+                            <td>@if($event->projects->isNotEmpty())
+                                    @foreach ($event->projects as $singleProjects)
+                                        <span>{{ $singleProjects->name }}</span>
+                                    @endforeach
+                                @else
+                                    General
+                                @endif
+                            </td>
+                            <td>{{ $event->location }}</td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.table-responsive -->
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer text-center">
+            <a href="{{ url('admin/calendar') }}" class="uppercase">View Calendar</a> or
+            <a href="{{ url('admin/calendars') }}" class="uppercase">View Event List</a>
+        </div>
+        <!-- /.box-footer -->
+    </div>
+@endsection
